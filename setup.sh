8cac234b2a9547b370b09022ee01a85ec823fef5
Boon-yalit <144957308+Boon-yalit@users.noreply.github.com>
2024-01-26 02:44:47 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,14 +9,16 @@ apt-get install automake -y
 apt-get install autotools-dev -y  
 apt-get install build-essential -y
 apt-get install nano -y
-pip2 install schedule
 
 
 chmod +x edit-miner
 chmod +x run-miner
 chmod +x add-ip
 chmod +x update
-chmod +x up-grade
+chmod +x down-grade
+chmod +x ANSI_Shadow.flf
+chmod +x backup
+chmod +x restore
 
 apt-get install python3 -y
 apt-get install pip -y
@@ -25,13 +27,15 @@ apt-get install figlet -y
 apt-get install python3-progress -y
 apt-get install python3-requests -y
 
-
 mv mobile-mining ../../etc
 mv edit-miner ../../bin
 mv run-miner ../../bin
 mv add-ip ../../bin
 mv update ../../bin
-mv up-grade ../../bin
+mv down-grade ../../bin
+mv ANSI_Shadow.flf ../../usr/share/figlet
+mv backup /data/data/com.termux/files/usr/bin
+mv restore /data/data/com.termux/files/usr/bin
 
 run-miner
 
